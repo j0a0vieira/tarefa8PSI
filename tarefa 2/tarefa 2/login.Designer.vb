705fd447324e68b2f2a82531e00f54608ab47a6e
vieiraCoder <56136565+vieiraCoder@users.noreply.github.com>
2020-05-21 17:51:28 +0100

    Add files via upload

---
+++ tarefa 2/tarefa 2/login.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(login))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtLogin = New System.Windows.Forms.TextBox()
+        Me.txtSenha = New System.Windows.Forms.TextBox()
+        Me.btnEntrar = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(73, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(147, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Blue
+        Me.Label2.Location = New System.Drawing.Point(73, 166)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(142, 31)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password"
+        '
+        'txtLogin
+        '
+        Me.txtLogin.ForeColor = System.Drawing.Color.Blue
+        Me.txtLogin.Location = New System.Drawing.Point(39, 76)
+        Me.txtLogin.Name = "txtLogin"
+        Me.txtLogin.Size = New System.Drawing.Size(232, 20)
+        Me.txtLogin.TabIndex = 2
+        '
+        'txtSenha
+        '
+        Me.txtSenha.ForeColor = System.Drawing.Color.Blue
+        Me.txtSenha.Location = New System.Drawing.Point(39, 215)
+        Me.txtSenha.Name = "txtSenha"
+        Me.txtSenha.Size = New System.Drawing.Size(232, 20)
+        Me.txtSenha.TabIndex = 3
+        '
+        'btnEntrar
+        '
+        Me.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEntrar.ForeColor = System.Drawing.Color.Blue
+        Me.btnEntrar.Location = New System.Drawing.Point(12, 276)
+        Me.btnEntrar.Name = "btnEntrar"
+        Me.btnEntrar.Size = New System.Drawing.Size(277, 66)
+        Me.btnEntrar.TabIndex = 6
+        Me.btnEntrar.Text = "Logar"
+        Me.btnEntrar.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(337, 73)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(201, 234)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(574, 365)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnEntrar)
+        Me.Controls.Add(Me.txtSenha)
+        Me.Controls.Add(Me.txtLogin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "login"
+        Me.Text = "login"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtLogin As TextBox
+    Friend WithEvents txtSenha As TextBox
+    Friend WithEvents btnEntrar As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ tarefa 2/tarefa 2/login.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Data.SqlClient
+
+Public Class login
+
+    Dim tau As New Form1
+    Private Sub login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtLogin.Select()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnEntrar.Click
+
+        Dim connString As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Wintarefa.mdf;
+                                    Integrated Security=True;Connect Timeout=30"
+
+        Dim ligacao As SqlConnection = New SqlConnection(connString)
+
+        Try
+            ligacao.Open()
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
+
+        Dim cmd As SqlCommand = New SqlCommand("SELECT * FROM Alunos WHERE login = '" &
+                                               txtLogin.Text & "' AND senha = '" & txtSenha.Text & "'", ligacao)
+
+        Dim dr As SqlDataReader = cmd.ExecuteReader
+        Dim encontrou As Boolean = False
+        Dim strNome As String = ""
+        If dr.HasRows Then
+            While dr.Read
+                encontrou = True
+                strNome = dr("nome").ToString
+            End While
+        End If
+
+        ligacao.Close()
+
+        If encontrou = True Then
+            Me.Hide()
+            tau.Show()
+            tau.Label1.Text = "Bem vindo " & strNome
+        Else
+            Dim msg As String = "Não encontrado." & vbNewLine & "Utilizador ou senha incorretos."
+            Dim titulo As String = "Aviso!"
+            Dim botoes = MessageBoxButtons.OK
+            Dim icone = MessageBoxIcon.Exclamation
+            MessageBox.Show(msg, titulo, botoes, icone)
+            txtLogin.Text = ""
+            txtSenha.Text = ""
+        End If
+
+    End Sub
+End Class
